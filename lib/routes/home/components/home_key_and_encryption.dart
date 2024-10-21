ddccc873524368cf85a7d7f9e68b46e99f35761b
zahidayturan <21060607@stu.omu.edu.tr>
2024-10-21 23:53:15 +0300

    on tap widget

--- lib/routes/home/components/home_key_and_encryption.dart
+++ lib/routes/home/components/home_key_and_encryption.dart
@@ -96,23 +96,28 @@ class _HomeKeyAndEncryptionState extends State<HomeKeyAndEncryption> {
           bottom: -6,
           child: Image.asset("assets/icons/allKeys.png",height: 56,color: Theme.of(context).scaffoldBackgroundColor),
         ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              RichTextWidget(
-                  fontSize: 14,
-                  texts: const ["Üretilen\n","Anahtarlar"],
-                  colors: [colors.greenDark],
-                  fontFamilies: const ["FontMedium","FontBold"]
+        onTapWidget(
+            onTap: (){
+
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  RichTextWidget(
+                      fontSize: 14,
+                      texts: const ["Üretilen\n","Anahtarlar"],
+                      colors: [colors.greenDark],
+                      fontFamilies: const ["FontMedium","FontBold"]
+                  ),
+                  const Align(
+                      alignment: Alignment.bottomRight,
+                      child: RegularText(texts: "Üretilmiş\nanahtar\nyok",size: 11,align: TextAlign.end,))
+                ],
               ),
-              const Align(
-                  alignment: Alignment.bottomRight,
-                  child: RegularText(texts: "Üretilmiş\nanahtar\nyok",size: 11,align: TextAlign.end,))
-            ],
-          ),
+            )
         ),
       ],
     ));
@@ -129,30 +134,33 @@ class _HomeKeyAndEncryptionState extends State<HomeKeyAndEncryption> {
               bottom: -14,
               child: Image.asset("assets/icons/generateKey.png",height: 102,color: Theme.of(context).scaffoldBackgroundColor),
             ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    BaseContainer(
-                        color: colors.greenDark,
-                        radius: 50,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: RichTextWidget(
-                              texts: const ["Anahtar ","Üret"],
-                              fontSize: 14,
-                              colors: [colors.white],
-                              fontFamilies: const ["FontMedium","FontBold"]),
-                        )),
-                    const RegularText(texts: "Aktif anahtar\nyok",size: 11,align: TextAlign.end,)
-                  ],
+            onTapWidget(
+                onTap: (){},
+                child:  Padding(
+                padding: const EdgeInsets.all(10),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      BaseContainer(
+                          color: colors.greenDark,
+                          radius: 50,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: RichTextWidget(
+                                texts: const ["Anahtar ","Üret"],
+                                fontSize: 14,
+                                colors: [colors.white],
+                                fontFamilies: const ["FontMedium","FontBold"]),
+                          )),
+                      const RegularText(texts: "Aktif anahtar\nyok",size: 11,align: TextAlign.end,)
+                    ],
+                  ),
                 ),
-              ),
-            ),
+              )
+            )
           ],
         ));
   }
@@ -169,13 +177,8 @@ class _HomeKeyAndEncryptionState extends State<HomeKeyAndEncryption> {
                   turns: const AlwaysStoppedAnimation(30 / 360),
                   child: Image.asset("assets/icons/addFile.png",height: 90,color: Theme.of(context).scaffoldBackgroundColor)),
             ),
-            Material(
-              type: MaterialType.transparency,
-              child: InkWell(
-                onTap: () {
-
-                },
-                borderRadius: BorderRadius.circular(8),
+            onTapWidget(
+                onTap: (){},
                 child: Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Column(
@@ -202,11 +205,20 @@ class _HomeKeyAndEncryptionState extends State<HomeKeyAndEncryption> {
                       const Center(child: RegularText(texts: "Dosya seçmek için dokunun",size: 13,)),
                       const SizedBox(height: 12,),
                     ],),
-                ),
-              ),
-            )
+                ))
           ],
         ));
   }
 
+  Widget onTapWidget({required void Function()? onTap, required Widget child}) {
+    return Material(
+      type: MaterialType.transparency,
+      borderRadius: BorderRadius.circular(8),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(8),
+        child: child,
+      ),
+    );
+  }
 }
